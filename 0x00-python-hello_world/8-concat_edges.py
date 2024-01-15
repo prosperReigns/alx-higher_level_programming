8faa4598dde047429dbb27aebc8c36455c4eec9d
prosperReigns <prosperkinging7@gmail.com>
2024-01-15 12:28:19 -0800

    pyhton script

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67], end="")
+print(str[-22:-18], end=" ")
+print(str[:6], end="\n")
+
