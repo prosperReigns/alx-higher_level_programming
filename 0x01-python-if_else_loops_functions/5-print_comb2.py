a262122771c64bb28b78e8115dec669a5d59160e
prosperReigns <prosperkinging7@gmail.com>
2024-01-16 11:43:29 -0800

    complete mandatory projects

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print(f"{number:02d}", end="\n")
-    else:
-        print(f"{number:02d}", end=", ")
+for number in range(0, 99):
+    print('{:02d}, '.format(num), end='')
+print('99')
