1b1c0836032261a33ba7aeeab096d69c94840647
prosperReigns <prosperkinging7@gmail.com>
2024-01-20 00:24:41 -0800

    fix bug in file 11,0,2,1,4,3, and 9

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for integer in my_list:
-        print("{}".format(integer))
+    for i in my_list:
+        print("{:d}".format(i))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(1, len(my_list)):
-        print("{}".format(my_list[-i]))
-    print("{}".format(my_list[0]))
+    for i in my_list[::-1]:
+        print("{:d}".format())
