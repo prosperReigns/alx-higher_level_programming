f9a00767c40b6e83f6f0b9a6b3975af5b1bea517
prosperReigns <prosperkinging7@gmail.com>
2024-01-20 00:06:58 -0800

    fix bug in 6,7,9,11 files

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    print("{}".format(my_list))
+    for i in range(1, len(my_list)):
+        print("{}".format(my_list[-i]))
+    print("{}".format(my_list[0]))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,13 +1,11 @@
 #!usr/bin/python3
 
 def max_integer(my_list=[]):
-    if my_list:
-        number = my_list[0]
-        list_len = len(my_list)
-        for j in range(0, list_len - 1):
-            if number < my_list[j + 1]:
-                number = my_list[j + 1]
-
-        return number
-    else:
+    if len(my_list) < 0:
         return None
+    else:
+        maximum = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > maximum:
+                maximum = my_list[i]
+        return maximum
