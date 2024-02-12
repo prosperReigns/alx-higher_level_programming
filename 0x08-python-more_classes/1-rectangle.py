43b7d67a6e05bbd0fe13ba394e185cfe6d1beec8
prosperReigns <prosperkinging7@gmail.com>
2024-02-12 18:43:14 +0100

    set the height and width of rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A module for a rectangle"""
+
+class Rectangle:
+    """represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the current width of the rectangle"""
+        return (self.__width)
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the current height of the rectangle"""
+        return (self.__height)
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
