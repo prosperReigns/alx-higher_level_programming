d526115b4433b371ae8241f7482f322a21c3fda2
prosperReigns <prosperkinging7@gmail.com>
2024-04-22 10:39:09 -0700

    solution

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let counter = 0;
+while (counter < sentence.lenght){
+    console.log(sentence[counter]);
+    counter++;
+}
